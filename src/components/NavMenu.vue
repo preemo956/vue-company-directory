dd7cb338529e674d99602ce1d3cd7bccdc811f26
Alex Garza <preemo956@gmail.com>
2023-09-12 19:17:53 -0500

    second comit

--- src/components/NavMenu.vue
+++ src/components/NavMenu.vue
@@ -1,16 +1,43 @@
+<script setup>
+import { ref } from 'vue';
+const brand = ref ('A.J.s AV Company Directory')
+</script>
+
 <template>
-  <nav>
-    <RouterLink to="/">Home</RouterLink>
-    <RouterLink to="/other">Other</RouterLink>
+  <nav class="bg-blue-900">
+    <div class="wrapper">
+      <div class="brand">
+        <span class="brand-title">{{ brand }}</span>
+    </div>
+      <div class="menu">
+          <a href="#" class="menu-item"> Departments </a>
+          <a href="#" class="menu-item"> Settings </a>
+          <a href="#" class="menu-item"> Logout </a>
+      </div>  
+    </div>
+
   </nav>
 </template>
 
 <style lang="postcss" scoped>
 nav {
-  @apply flex justify-center space-x-4;
+  @apply flex h-20 bg-blue-900 text-slate-200 justify-between space-x-4;
 
+  .wrapper {
+    @apply container mx-auto flex w-full items-center justify-between;
+  }
   & .router-link-active {
     @apply underline underline-offset-4;
+  }
+  .brand {
+    &-title{
+      @apply text-2xl font-bold text-red-600;
+    }
+  }
+  .menu{
+    &-item{
+    @apply hover:bg-slate-200 flex gap-1 text-lg font-bold text-red-600 rounded-md;
+    }
   }
 }
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,10 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Hello There ✋')
+const title = ref('Main Content!!!')
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
+  <main class="flex min-h-screen ">
     <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
   </main>
 </template>
